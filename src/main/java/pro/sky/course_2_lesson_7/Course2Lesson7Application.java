def9a36eea0ef5dd5243a8e8e3c376b413ddb0f3
Alexey <lexshadrin@mail.ru>
2022-07-04 18:36:21 +1100

    Начало ДЗ_7

---
+++ src/main/java/pro/sky/course_2_lesson_7/Course2Lesson7Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course_2_lesson_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Lesson7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Lesson7Application.class, args);
+    }
+
+}
